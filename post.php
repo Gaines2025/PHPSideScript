0a5425fa06acff8ddf89c30ba1dabae67a3d7329
Gaines2025 <maalakai.gaines@ou.edu>
2024-09-15 16:11:32 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,1 +1,12 @@
-
+<?php
+$pageTitle = "Post";
+include "view-header.php";
+?>
+<h1>Home!</h1>
+<form method="post" action="post-result.php>
+  <input type="text" name="my-name">
+  <input type="submit" value="Post data">
+  </form>
+<?php
+include "view-footer.php";
+?>
