03e01330f2a89b282c3e0aee5c83223da11a27c6
Gaines2025 <maalakai.gaines@ou.edu>
2024-09-16 13:32:17 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -6,14 +6,16 @@ include "view-header.php";
 <?php
 echo getDisplay();
 include "view-footer.php";
-?>
 
-function getDisplay() {
-    if (isset($_POST['my-name'])) {
+function getDisplay() 
+{
+    if (isset($_POST['my-name'])) 
+    {
     return "<p>The value sent is: </p>" . $_POST['my-name'];
-} else {
-  return "<p>Nothing posted to the page.</p>";
-<?php
+    } 
+    else 
+    {
+      return "<p>Nothing posted to the page.</p>";
 }
 }
 
