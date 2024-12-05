d1feead8d25377069ba4e4d87d15fbc866a7d783
Gaines2025 <maalakai.gaines@ou.edu>
2024-12-04 18:17:29 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -31,6 +31,9 @@
          <li class="nav-item">
           <a class="nav-link" href="get-result.php">Get result</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javaFunctions.html">JavaFunctions</a>
+        </li>
       </ul>
     </div>
   </div>
